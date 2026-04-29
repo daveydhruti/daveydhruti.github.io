649ec3e7eaad3ead416cfb58482cc35cc63aec54
Dhruti Davey <daveydhruti@duck.com>
2026-04-29 18:28:10 +0100

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/documents");
+
+
+  // Use JSON data files from _data directory
+  eleventyConfig.addDataExtension("json", (contents) => JSON.parse(contents));
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    }
+  };
+};
---
+++ src/about/about.11tydata.js
@@ -0,0 +1,14 @@
+module.exports = {
+  intro: "I recently graduated from the University of Sheffield, where I did my Master's in Cybersecurity and AI. Currently exploring Neuromorphic Computing.",
+  education: [
+    { degree: "M.Sc. Cybersecurity and Artificial Intelligence", school: "University of Sheffield", period: "2024\u20132025", thesis: "SNN model of Hippocampal Reverse Replay for Reinforcement Learning", grade: "Distinction" },
+    { degree: "B.Sc. in Computer Science with Honours", school: "Heriot-Watt University, Dubai", period: "2021\u20132024", thesis: "Large Language Model-based Intrusion Detection", grade: "First-class" }
+  ],
+  experience: [
+    { role: "Teaching Assistant", org: "Heriot-Watt University, Dubai", period: "Sept. 2023\u2013April 2024" },
+    { role: "Software Engineering Intern", org: "DevelopL", period: "Aug. 2023\u2013Oct. 2023" }
+  ],
+  // interests: [
+  //   "climbing"
+  // ]
+};
