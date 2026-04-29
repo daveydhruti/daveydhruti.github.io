3fb85e2d36062adf03bcc270f92636a3c33ef37b
Dhruti Davey <daveydhruti@duck.com>
2026-04-29 18:53:20 +0100

    .gitlab-ci.yml added

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "public",
       includes: "_includes",
       data: "_data"
     }
