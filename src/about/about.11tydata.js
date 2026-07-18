9ceed207f69ef814146b790e5d4e93f29ea49025
Dhruti Davey <daveydhruti@duck.com>
2026-07-18 18:47:47 +0100

    about updated

--- src/about/about.11tydata.js
+++ src/about/about.11tydata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  intro: "I recently graduated from the University of Sheffield with an M.Sc. in Cybersecurity and AI. I am currently exploring roles in AI Engineering and Research, while independently investigating spiking neural networks as I prepare for Ph.D. applications in neuromorphic algorithms.",
+  intro: "I recently graduated from the University of Sheffield with an M.Sc. in Cybersecurity and AI. I am currently looking for roles in AI Engineering and Research, while independently studying spiking neural networks as I prepare for Ph.D. applications in neuromorphic algorithms.",
   body: [
     "These are the modules I took during my undergraduate and master's.",
   ],
