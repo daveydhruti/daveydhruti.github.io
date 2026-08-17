2e37ea010555f8573cb00797fae946a059d93e83
Dhruti Davey <daveydhruti@duck.com>
2026-08-17 13:30:41 +0100

    remove project description, change about, remove writings

--- src/index.11tydata.js
+++ src/index.11tydata.js
@@ -1,3 +1,3 @@
 module.exports = {
-  intro: "I recently graduated from the University of Sheffield with an M.Sc. in Cybersecurity and AI. I am currently looking for roles in AI Engineering and Research, while independently studying spiking neural networks as I prepare for Ph.D. applications in neuromorphic algorithms.",
+  intro: "Prospective Ph.D. student with a research interest in neuromorphic algorithms. Graduated from the University of Sheffield with a Master's in Cybersecurity and AI.",
 };
